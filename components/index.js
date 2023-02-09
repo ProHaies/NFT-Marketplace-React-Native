f6e57280f19e8642f359b9c424bf7feea213a6c1
Borislav Stefanov <prohaies3101@gmail.com>
2023-02-09 14:03:53 +0200

    first commit

--- components/index.js
+++ components/index.js
@@ -2,6 +2,9 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
 export {
-    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+   SubInfo, DetailsBid, HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton, DetailsDesc
 }
--- screens/Home.js
+++ screens/Home.js
@@ -3,17 +3,35 @@ import { View, SafeAreaView, FlatList, Text } from "react-native";
 import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (value.length === 0) {
+      setNftData(NFTData);
+    }
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length === 0) {
+      setNftData(NFTData);
+    } else {
+      setNftData(filteredData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar backgroundColor={COLORS.black} />
       <View style={{ flex: 1 }}>
-        <View style={{ zIndex: 0 }}>
+      <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader/>}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
 
